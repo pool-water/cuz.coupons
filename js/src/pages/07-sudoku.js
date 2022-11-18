0d7abeb32ae214c95a74e32e277686c62ad03052
Matt 3 <not.mattowen@gmail.com>
2022-11-18 15:25:41 -0500

    :shaved_ice: throw a gradient on if it's boring

--- js/src/pages/07-sudoku.js
+++ js/src/pages/07-sudoku.js
@@ -1,5 +1,7 @@
 import {Page} from "./page.js";
 
+import seedrandom from "seedrandom";
+
 function generateTable(n, className) {
   const table = document.createElement("TABLE");
   for (let i=0; i < n; i++) {
@@ -19,6 +21,8 @@ export class SudokuPage extends Page {
   constructor(el, seed) {
     super(el, seed);
 
+    let prng = new seedrandom(this.seed);
+
     const container = el;
     const grid = generateTable(3, "big");
     grid.name = grid.id = "sudoku";
@@ -35,6 +39,11 @@ export class SudokuPage extends Page {
         input.size = 1;
         input.type = "tel";
         input.className = "sudoku-entry";
+
+        if (prng() < 0.5) {
+          input.value = Math.floor(1+8*prng());
+        }
+
 
         cell.appendChild(input);
       });
