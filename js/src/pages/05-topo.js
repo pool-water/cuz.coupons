b87dee03a40bd2763271f50b1208ba934feedad1
Austin Pickett <pickett.austin@gmail.com>
2022-12-09 00:30:50 -0500

    😈

---
+++ js/src/pages/05-topo.js
@@ -0,0 +1,45 @@
+import { TopoApp } from "@pool-water/secret-sauce";
+import { Page } from "./page.js";
+
+import seedrandom from "seedrandom";
+
+/**
+ *
+ */
+export class TopoPage extends Page {
+  constructor(el, seed) {
+    super(el, seed);
+
+    const canvas = document.createElement("CANVAS");
+    canvas.className = "backdrop";
+    canvas.width = el.clientWidth;
+    canvas.height = el.clientHeight;
+    this.el.appendChild(canvas);
+
+    this.app = new TopoApp();
+  }
+
+  update() {
+    this.app.update();
+  }
+
+  draw() {
+    this.app.draw();
+  }
+
+  clear() {
+    this.el.querySelectorAll(":scope > canvas").forEach((v) => {
+      this.el.removeChild(v);
+    });
+  }
+
+  features() {
+    let prng = new seedrandom(this.seed);
+    return {
+      pos: [0.1, 0.0, 0.0],
+      sigma: 10.0,
+      rho: 10.74 + prng() * 20.0,
+      beta: 8.0 / 3.0,
+    };
+  }
+}
